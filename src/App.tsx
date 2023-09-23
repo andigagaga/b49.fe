255d4d98cacd993cef64d82563d172c23fb991a9
andigagaga <andigagaga1@gmail.com>
2023-09-23 07:38:50 +0700

    class-component-introduction

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Perjuangan from "./Pdip";
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Perjuangan />
+            </div>
+        )
+    }
+} export default App;
---
+++ src/Pdip.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Perjuangan extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>apa sih kamu</h1>
+            </div>
+        )
+    }
+}
